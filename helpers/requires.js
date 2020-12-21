ed725c5e81e9b31b6fcb23d2c503f67193bc9539
axyorah <elvira.bozileva@gmail.com>
2020-12-21 23:52:39 +0100

    rm: removing unnecessary requires

--- helpers/requires.js
+++
@@ -1,4 +0,0 @@
-// required modules can only be used if bundled version of this file was made with `browserify`
-var OrbitControls = require('three-orbit-controls')(THREE); // allows to control camera position
-var PdeUtils = require('../utils/pdeutils.js'); // custom functions for solving PDEs
-var SnailUtils = require('../utils/snailutils.js'); // custom functions for setting up snail shell geometry/textures
