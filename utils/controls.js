bd0530a9698e6830a4477dd7599fa85196829f34
axyorah <elvira.bozileva@gmail.com>
2020-12-22 01:37:34 +0100

    mod: minor gui style changes

--- utils/controls.js
+++ utils/controls.js
@@ -1,19 +1,24 @@
+var gui = new dat.GUI();
+gui.domElement.style.marginTop = "10px";
+gui.domElement.children[1].style.opacity = "0.9";
+
+// init values for gui controller
+effectController = {
+    raddecay: 0.3,
+    turns: 5,
+
+    texlongrepeats: 4.7,
+    textangrepeats: 2,
+    textangoffset: 0.,
+    texname: "angelfish0",
+
+    f: 0.0140,
+    k: 0.0450
+};
+
 function setupGui() {
-
-    effectController = {
-        raddecay: 0.3,
-        turns: 5,
-
-        texlongrepeats: 4.7,
-        textangrepeats: 2,
-        textangoffset: 0.,
-        texname: "angelfish0",
-
-        f: 0.0140,
-        k: 0.0450
-    };
-
-    var gui = new dat.GUI();
+    
+    var h;    
     h = gui.addFolder("Geometry");
     h.add( effectController, "raddecay", 0.0, 1.0, 0.01).name("radius decay");
     h.add( effectController, "turns", 0.1, 10.0, 0.1).name("#turns");
